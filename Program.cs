2d1676b1b747d4279a9ee6c3894df7f88bbbac4e
Joel Southall <vextechmage@gmail.com>
2025-03-21 01:14:43 -0700

    Add basic indexedDB setup and Game Asset CRUD

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
 using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 using GameAssetTracker;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Just the built-in HttpClient
-builder.Services.AddScoped(sp => new HttpClient
-{
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-});
+// Register IndexedDB Service
+builder.Services.AddScoped<IndexedDbService>();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
---
+++ Services/IndexedDbService.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+public class IndexedDbService
+{
+    private readonly IJSRuntime _jsRuntime;
+    private const string DbName = "GameAssetTrackerDB";
+    private const string StoreName = "Assets";
+
+    public IndexedDbService(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public async Task InitializeDatabaseAsync()
+    {
+        await _jsRuntime.InvokeVoidAsync("indexedDBHelper.initDb", DbName, StoreName);
+    }
+
+    public async Task AddAssetAsync(GameAsset asset)
+    {
+        string assetJson = JsonSerializer.Serialize(asset);
+        await _jsRuntime.InvokeVoidAsync("indexedDBHelper.addItem", DbName, StoreName, assetJson);
+    }
+
+    public async Task<List<GameAsset>> GetAssetsAsync()
+    {
+        var json = await _jsRuntime.InvokeAsync<string>("indexedDBHelper.getAllItems", DbName, StoreName);
+        return string.IsNullOrEmpty(json) ? new List<GameAsset>() : JsonSerializer.Deserialize<List<GameAsset>>(json);
+    }
+
+    public async Task DeleteAssetAsync(int id)
+    {
+        await _jsRuntime.InvokeVoidAsync("indexedDBHelper.deleteItem", DbName, StoreName, id);
+    }
+}
